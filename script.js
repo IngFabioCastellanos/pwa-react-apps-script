f53c5d631987f2939c2d9d95daecc405b4b88f84
abhijithcmr <140905414+abhijithcmr@users.noreply.github.com>
2023-09-28 01:05:41 +0530

    Update script.js
    Updated to English language

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
     .register("./sw.js")
-    .then((reg) => console.log("Registro de SW exitoso ", reg))
-    .catch((err) => console.error("Error al tratar de registrar el SW ", err));
+    .then((reg) => console.log("SW registration successful", reg))
+    .catch((err) => console.error("Error when trying to register the SW", err));
 } else {
-  console.log("no hay serviceWorker");
+  console.log("No service work");
 }
